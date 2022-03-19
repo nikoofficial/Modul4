7fa37f27fedbdcfea22fd2f0745a8614497b3278
nikoofficial <anikorizki41@gmail.com>
2022-03-19 13:32:03 +0700

    implementasi generic method

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul4_1302204050
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Penjumlahan.JumlahTigaAngka<long>(12, 34, 56));
+        }
+    }
+    class Penjumlahan
+    {
+        public static T JumlahTigaAngka<T>(T input1, T input2, T input3)
+        dynamic a = input1;
+        dynamic b = input2;
+        dynamic c = input3;
+
+        return a + b + c;
+
+    }
+}
